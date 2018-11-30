25e0dc56acbfa3990a6625ecc427f8c14b62474e
tanbin <1370808424@qq.com>
2018-11-30 17:19:01 +0800

    tmp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
-<template lang="pug">
-  div#app
-    img(src="./assets/logo.png")
-    h1 Welcome from pug & sass
+<template>
+  <div class="wrap">
+      <TheHeader/>
+      <TheFooter/>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-}
+    import TheHeader from '@/components/layouts/TheHeader'
+    import TheFooter from '@/components/layouts/TheFooter';
+    export default {
+        name: 'app',
+        components: {
+            TheHeader,
+            TheFooter
+        }
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-
-  h1 {
-    color: #42b983;
-  }
-}
+    $container-large-desktop: 1200px;
+    $btn-primary-bg: #00b5ad;
+    $btn-primary-border: #00b5ad;
+    $label-primary-bg: #00b5ad;
+    $pagination-active-bg: #00b5ad;
+    $pagination-active-border: #00b5ad;
+    $pagination-color: #00b5ad;
+    $input-border-focus: #00b5ad;
+    $link-color: #12c4c5;
+    $link-hover-color: #22ddde;
+    $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+    $fa-font-path: "~font-awesome/fonts/";
+    @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+    @import "~font-awesome/scss/font-awesome";
+    @import "./styles/main";
+    @import "./styles/extra";
 </style>
