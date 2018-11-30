fa6a8ea421b5cb0c9899e3b7a0b0fed530677cb3
tanbin <1370808424@qq.com>
2018-11-30 09:58:25 +0800

    add github pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/vuejs-essential/dist/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
